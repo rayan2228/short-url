b7cbb04742fa8766a4823ac5d587039b3941baa6
rayan2228 <rayanh.dev@gmail.com>
2025-02-08 18:30:37 +0600

    feat: add login functionality with validation and password comparison

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,5 +1,5 @@
-import { User } from "../models/users.schema.js";
 import bcrypt from "bcryptjs";
+import { User } from "../models/users.schema.js";
 const createUser = async (req, res) => {
   try {
     const { displayname, email, password } = req.body;
@@ -22,4 +22,27 @@ const createUser = async (req, res) => {
   }
 };
 
-export { createUser };
+const loginUser = async (req, res) => {
+  try {
+    if (Object.values(req.body).some((value) => !value?.trim())) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "credentials are incorrect" });
+    }
+    const isPasswordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordMatch) {
+      return res.status(400).json({ message: "credentials are incorrect" });
+    }
+    return res
+      .status(200)
+      .json({ message: "User logged in successfully", user });
+  } catch (error) {}
+};
+
+export { createUser, loginUser };
+
